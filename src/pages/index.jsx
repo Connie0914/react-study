c5f6d09920495dac32cc315e813cc481c0ddc22e
Connie0914 <solveriddles@gmail.com>
2021-09-17 10:37:19 +0900

    :sparkles: counterを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer"
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
   //* 引数がなかったり、煩雑にならない場合は外に書く.
   //* 再レンダリング時にメソッドが再生成されず、挙動が早いため.
@@ -14,15 +14,16 @@ import { useCallback, useEffect } from 'react'
   // }, []);
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1)
+  // let foo = 1;
 
   //* 簡潔に書ける.
   //* useCallback:再レンダリングされた時に再生成されないようにする.
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href)
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    // foo = foo + 1;
+  };
 
   useEffect(() => { //* マウント時の処理
     document.body.style.backgroundColor = "lightblue";
@@ -31,18 +32,15 @@ export default function Home() {
     }
   }, []);
 
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <a
-        href="/about"
-        onClick={handleClick}
-      >
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button  onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
